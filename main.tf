4670424a1305f063360753216b78550ed6c092d8
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-25 21:51:25 +0200

    comments

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 # simple file resource
-#resource names must not be the same.
+#resource local names must not be the same.
+
+# terraform plan -out=./plan    terraform apply ./plan
 
 resource "local_file" "tf_example" {
     filename = "${path.module}/example.txt" 
     content = "Hey this is updated content to write to the file"
+    #count = 3
 }
 
 resource "local_sensitive_file" "tf_example2" {
