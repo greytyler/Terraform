fd9225136c860ac4f5589b9e1646b59ae801de6b
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-25 21:33:01 +0200

    sensitive file

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 # simple file resource
 resource "local_file" "tf_example" {
     filename = "${path.module}/example.txt" 
+    content = "Hey this is updated content to write to the file"
+}
+
+resource "local_sensitive_file" "tf_example2" {
+    filename = "${path.module}/sensitive.txt"
+    content = "This is a sensitive file created by Terraform"
 }
