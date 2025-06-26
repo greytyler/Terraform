297e10b96c2a818a8205a977f4aa6dbba3afcfe6
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-27 00:11:23 +0200

    Update main.tf

--- State/main.tf
+++ State/main.tf
@@ -14,4 +14,4 @@ resource "local_sensitive_file" "tf_sensitive" {
 #terraform destroy -target=local_sensitive_file.tf_sensitive 
 # terraform state -h .......see tf advenced state management commands
 #terraform refresh to check for state drift, if change done on console was intentional tf will update the state file instead of updating the resources
-# it is recommended to keep the state file in a remote backend like s3, azurerm, gcs, or in a seperate folder etc.
+# it is recommended to keep the state file in a remote backend like s3, azurerm, gcs, or in a seperate folder if local etc.
