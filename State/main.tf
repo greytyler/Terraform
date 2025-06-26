5006b33af1b5ca2b20fdbad1e3590c46f29a09ef
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-26 22:49:07 +0200

    Update main.tf

--- State/main.tf
+++ State/main.tf
@@ -2,11 +2,11 @@ resource "local_file" "tf_file1" {
     filename = "${path.module}/fileum.txt" 
     content = "This is demo content"
 }
-# resource "local_file" "tf_file2" {
-#     filename = "${path.module}/fileum2.txt" 
-#     content = "This is demo content"
-# }
-# resource "local_file" "tf_sensitive" {
-#     filename = "${path.module}/sensitive.md" 
-#     content = "#%Pass32427^-0"
-# }
+resource "local_file" "tf_file2" {
+    filename = "${path.module}/fileum2.txt" 
+    content = "This is demo content"
+}
+resource "local_file" "tf_sensitive" {
+    filename = "${path.module}/sensitive.md" 
+    content = "#%Pass32427^-0"
+}
