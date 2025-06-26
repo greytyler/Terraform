59aeb04d835a187443a2b6d50cecd7780e6d462a
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-26 22:25:50 +0200

    inititialize

---
+++ State/main.tf
@@ -0,0 +1,12 @@
+resource "local_file" "tf_file1" {
+    filename = "${path.module}/fileum.txt" 
+    content = "This is demo content"
+}
+resource "local_file" "tf_file2" {
+    filename = "${path.module}/fileum2.txt" 
+    content = "This is demo content"
+}
+resource "local_file" "tf_sensitive" {
+    filename = "${path.module}/sensitive.md" 
+    content = "#%Pass32427^-0"
+}
