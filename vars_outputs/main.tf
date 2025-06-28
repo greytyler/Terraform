f728c1f94b275096809a3f1aa2c9135ea5abc603
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-28 12:11:25 +0200

    began folder for tf variables and outputs

---
+++ vars_outputs/main.tf
@@ -0,0 +1,12 @@
+esource "local_file" "example1" {
+    filename = "${path.module}/example1.txt" 
+    content = "This is demo content"
+}
+resource "local_file" "example2" {
+    filename = "${path.module}/example2.txt" 
+    content = "This is demo content"
+}
+resource "local_sensitive_file" "tf_sensitive" {
+    filename = "${path.module}/sensitive.md" 
+    content = "#%Pas#%6;9-0"
+}
