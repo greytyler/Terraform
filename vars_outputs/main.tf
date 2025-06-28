c176aa0dd52278192748631a13abd5bc1d5418e7
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-28 12:30:40 +0200

    Update main.tf

--- vars_outputs/main.tf
+++ vars_outputs/main.tf
@@ -1,12 +1,13 @@
-esource "local_file" "example1" {
-    filename = "${path.module}/example1.txt" 
+
+resource "local_file" "example1" {
+    filename = "${path.module}/${var.filename1}.txt" 
     content = "This is demo content"
 }
 resource "local_file" "example2" {
-    filename = "${path.module}/example2.txt" 
+    filename = "${path.module}/${var.filename2}.txt" 
     content = "This is demo content"
 }
 resource "local_sensitive_file" "example3" {
-    filename = "${path.module}/example3.md" 
+    filename = "${path.module}/${var.filename3}.md" 
     content = "#%Pas#%6;9-0"
 }
