cc38a47ef4144883acae35955effded630cb5d83
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-28 12:12:43 +0200

    change file names

--- vars_outputs/main.tf
+++ vars_outputs/main.tf
@@ -6,7 +6,7 @@ resource "local_file" "example2" {
     filename = "${path.module}/example2.txt" 
     content = "This is demo content"
 }
-resource "local_sensitive_file" "tf_sensitive" {
-    filename = "${path.module}/sensitive.md" 
+resource "local_sensitive_file" "example3" {
+    filename = "${path.module}/example3.md" 
     content = "#%Pas#%6;9-0"
 }
